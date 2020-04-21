183c9af822204e4988cb88352d0aa7eb8c1b6c6d
Brandon Little <brandonlittle1999@hotmail.com>
2020-04-21 14:54:44 +0100

    Finalised the coin flip code so that the programme will work when a user types in their input.

---
+++ README.md
@@ -0,0 +1,5 @@
+#Overiew
+
+#What I learnt: 
+
+
--- coin-flip.py
+++ coin-flip.py
@@ -1,40 +1,73 @@
 import math, random
 
+userChoice = ''
+computerChoice = ''
+answer = ''
+currentUserScore = 0
+currentComputerScore = 0
+totalUserScore = 0 
+totalComputerScore = 0
+
+print('Welcome to the coin flip game. You will play against a computer to see who has the best luck!')
+
 def beginGame():
-    print('Welcome to the coin flip game. You will play against a computer to see who has the best luck! Would you like to be Heads or Tails?')
+    global userChoice
+    global answer
+    global computerChoice
+    print('Would you like to be Heads or Tails?')
     userDecision = str(input())
-    userPlayer = ''
-    computerPlayer = ''
-    user = ''
-    computer = ''
 
     if userDecision.upper() == 'HEADS':
-        userPlayer = 'Heads'
-        computerPlayer = 'Tails'
+        userChoice = 'Heads'
+        computerChoice = 'Tails'
     elif userDecision.upper() == 'TAILS':
-        userPlayer = 'Tails'
-        computerPlayer = 'Heads'
+        userChoice = 'Tails'
+        computerChoice = 'Heads'
     else:
         print('Please state whether you want to be heads or tails')
         beginGame()
 
-    print('You have selected ' + userPlayer + ', so the computer will be ' + computerPlayer + '.')
+    print('You have selected ' + userChoice + ', so the computer will be ' + computerChoice + '.')
 
-def playGame(): 
     randomNumber = random.randint(0, 1)
-    print(randomNumber)
-    if randomNumber == 0:
+
+    if randomNumber == 0:#> 50:
         print('It\'s heads!')
-    elif randomNumber == 1:
+        answer = 'Heads'
+        resultScore()
+    elif randomNumber == 1:#randomNumber >= 1 and randomNumber <= 50:
         print('It\'s tails!')
+        answer = 'Tails'
+        resultScore()
 
+def resultScore():
+    global currentComputerScore
+    global totalComputerScore
+    global currentUserScore
+    global totalUserScore
+    global userChoice
+    global answer
+    global computerChoice
 
+    currentUserScore = 0
+    currentComputerScore = 0
 
+    if answer == userChoice:
+        currentUserScore = int(currentUserScore) + 1
+        totalUserScore = int(totalUserScore) + 1
+    elif answer == computerChoice:
+        currentComputerScore = int(currentComputerScore) + 1
+        totalComputerScore = int(totalComputerScore) + 1
 
-
-
+    if currentUserScore > currentComputerScore:
+        print('You have won this round and the computer has lost! Your total score so far is: ' + str(totalUserScore) + ' point(s) and the computers total score is: ' + str(totalComputerScore) + '\nWould you like to play again?')
+        playAgain = input()
+        if playAgain.upper() == 'YES':
+            beginGame()
+    elif currentComputerScore > currentUserScore:
+        print('You have lost this round and the computer has won! Your total score so far is: ' + str(totalUserScore) + ' point(s) and the computers total score is: ' + str(totalComputerScore) + '\nType yes to play again!')
+        playAgain = input()
+        if playAgain.upper() == 'YES':
+            beginGame()
 
 beginGame()
-playGame()
-
-
